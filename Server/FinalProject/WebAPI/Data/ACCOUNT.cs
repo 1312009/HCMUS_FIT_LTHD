54549e90cebb6d662db899a938a39a82e366d388
Nguyễn Tuấn Anh <tuananhpro2981995@gmail.com>
2016-11-07 18:13:54 +0700

    add new dataEntities to social login: Facebook. Gmail. Add webconfig appid, app secret

--- Server/FinalProject/WebAPI/Data/ACCOUNT.cs
+++ Server/FinalProject/WebAPI/Data/ACCOUNT.cs
@@ -18,6 +18,7 @@ namespace WebAPI.Data
         public ACCOUNT()
         {
             this.ACCOUNT_ROLE = new HashSet<ACCOUNT_ROLE>();
+            this.EXTERNALACCOUNTs = new HashSet<EXTERNALACCOUNT>();
         }
     
         public int ID { get; set; }
@@ -30,5 +31,7 @@ namespace WebAPI.Data
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ACCOUNT_ROLE> ACCOUNT_ROLE { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<EXTERNALACCOUNT> EXTERNALACCOUNTs { get; set; }
     }
 }
